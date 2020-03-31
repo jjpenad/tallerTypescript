4605821bc0a012d7b8d5196dfec7833b6b9ecfd9
Juan José Peña <jj.penad@uniandes.edu.co>
2020-03-30 22:31:00 -0500

    taller

---
+++ scripts/course.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Course = /** @class */ (function () {
+    function Course(name, professor, credits) {
+        this.name = name;
+        this.credits = credits;
+        this.professor = professor;
+    }
+    return Course;
+}());
+exports.Course = Course;
